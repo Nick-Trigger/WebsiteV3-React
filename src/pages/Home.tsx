36cb9ac7826908bea8970761a455d2fc8770a600
Nicholas Trigger <nst15@duke.edu>
2026-06-28 23:02:21 -0400

    update links on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <div className="text-xl py-1">Hey there 👋</div>
         <div className="text-5xl font-bold">I'm Nicholas Trigger</div>
         <div className="text-3xl py-3 font-bold">
-          Duke BME Alum '26 | Medical Devices, Embedded Systems & Imaging
+          Biomedical Engineer
         </div>
         <div className="py-2">
           <span className="text-lg">
@@ -34,39 +34,96 @@ export default function Home() {
             VentureWell grant and a provisional patent.
           </span>
         </div>
-        <div className="mt-8">
+        <div className="mt-8 flex w-full gap-2">
           <a
-            className="btn btn-outline btn-primary"
-            href="mailto:nicholas.trigger@duke.edu"
+            className="btn btn-outline btn-primary align-center"
+            href="mailto:nicktrig124@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Let's connect!
+            Let's Connect!
           </a>
-          <Link to="/projects" className="btn ml-5">
-            Portfolio
-          </Link>
           <a
+            href="https://duke.box.com/s/g4uf97sq9jq5zdsoe0rsb8g2djkvfpm2"
+            className="btn align-center"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: 'currentColor' }}
+            >
+              <path d="m12 16 4-5h-3V4h-2v7H8z"></path>
+              <path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z"></path>
+            </svg>
+          </a>
+          <a
+            className="btn align-center"
+            href="./projects"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portfolio
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="img"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: 'currentColor' }}
+            >
+              <path fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="1.5" d="M2.75 8.623v7.379a4 4 0 0 0 4 4h10.5a4 4 0 0 0 4-4v-5.69a4 4 0 0 0-4-4H12M2.75 8.624V6.998a3 3 0 0 1 3-3h2.9a2.5 2.5 0 0 1 1.768.732L12 6.313m-9.25 2.31h5.904a2.5 2.5 0 0 0 1.768-.732L12 6.313"></path>
+            </svg>
+          </a>
+          <a
+            className="btn align-center"
             href="https://www.linkedin.com/in/nick-trigger"
             target="_blank"
             rel="noopener noreferrer"
-            className="btn ml-5"
           >
             LinkedIn
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: 'currentColor' }}
+            >
+              <circle cx="4.983" cy="5.009" r="2.188"></circle>
+              <path d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"></path>
+            </svg>
           </a>
           <a
-            href="https://github.com/Nick-Trigger"
+            className="btn align-center"
+            href="https://github.com/nick-trigger"
             target="_blank"
             rel="noopener noreferrer"
-            className="btn ml-5"
           >
             GitHub
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style={{ fill: 'currentColor' }}
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
+              />
+            </svg>
           </a>
         </div>
       </div>
 
       <div>
-        <div className="text-3xl w-full font-bold mb-2">Some projects ⚙</div>
+        <div className="text-3xl w-full font-bold mb-2">Featured Projects ⚙</div>
       </div>
 
       <HorizontalCard
